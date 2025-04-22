3d0224d9638d8f13fa12dc74852a3e364abd3c2b
HardBoss07 <matteo@bosshard.net>
2025-04-22 15:11:26 +0200

    moved all styling to globals.css

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,31 +1,12 @@
 export default function Header() {
     return (
-        <header className="p-4 flex items-center justify-between border-b-2 mb-8">
-            <div className="flex items-center gap-x-4">
-                <a
-                    href="/"
-                >
-                    Home
-                </a>
-                <a
-                    href="/projects"
-
-                >
-                    Projects
-                </a>
-                <a
-                    href="/contact"
-                >
-                    Contact
-                </a>
+        <header>
+            <div>
+                <a href="/">Home</a>
+                <a href="/projects">Projects</a>
+                <a href="/contact">Contact</a>
             </div>
-            <a
-                href="https://github.com/HardBoss07"
-                target="_blank"
-                className="a-inverted"
-            >
-                GitHub
-            </a>
+            <a href="https://github.com/HardBoss07" target="_blank" className="a-inverted">GitHub</a>
         </header>
     );
 }
