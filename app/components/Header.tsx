b0f9ae052d355efca77ec63ef0138655bf80ca74
HardBoss07 <matteo@bosshard.net>
2025-04-22 10:40:44 +0200

    updated Header.tsx to centralized styling in globals.css

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,19 +4,17 @@ export default function Header() {
             <div className="flex items-center gap-x-4">
                 <a
                     href="/"
-                    className="text-3xl border-2 border-current px-6 py-0 rounded-full hover:bg-[#F207A8] hover:text-black transition-colors min-w-[12rem]"
                 >
                     Home
                 </a>
                 <a
                     href="/projects"
-                    className="text-3xl border-2 border-current px-6 py-0 rounded-full hover:bg-[#F207A8] hover:text-black transition-colors min-w-[12rem]"
+
                 >
                     Projects
                 </a>
                 <a
                     href="/contact"
-                    className="text-3xl border-2 border-current px-6 py-0 rounded-full hover:bg-[#F207A8] hover:text-black transition-colors min-w-[12rem]"
                 >
                     Contact
                 </a>
@@ -24,7 +22,7 @@ export default function Header() {
             <a
                 href="https://github.com/HardBoss07"
                 target="_blank"
-                className="text-right text-3xl border-2 border-current px-6 py-0 rounded-full transition-colors min-w-[12rem] bg-[#F207A8] text-black hover:bg-black hover:text-[#F207A8]"
+                className="a-inverted"
             >
                 GitHub
             </a>
