46df7a797480f174951ab3cc378b84666474df50
HardBoss07 <matteo@bosshard.net>
2025-04-25 11:10:17 +0200

    added content in contact/page.tsx added responsive design for contact/page.tsx in globals.css

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -8,6 +8,28 @@ export default function Contact() {
 
             <main>
                 <h1>Contact me</h1>
+                <p>
+                    If you're interested, shoot me a message with your inquiry on one of the chosen methods below. I will try to respond as quickly as possible, my timezone is GMT+1.
+                </p>
+                <h2>Social Media</h2>
+                <div className="contact">
+                    <a href="https://instagram.com/matteoluca07" target="_blank">Instagram - @matteoluca07</a>
+                    <a href="https://github.com/HardBoss07" target="_blank">GitHub - @HardBoss07</a>
+                    <a href="https://discord.com" target="_blank">Discord - @m4tt3o</a>
+                </div>
+
+                <h2>Personal Email</h2>
+                <div className="contact">
+                    <a href="mailto:matteo@bosshard.net">matteo@bosshard.net</a>
+                    <a href="mailto:matteobosshard@gmail.com">matteobosshard@gmail.com</a>
+                </div>
+
+                <h2>School Email</h2>
+                <div className="contact">
+                    <a href="mailto:matteo.bosshard@stud.kbw.ch">matteo.bosshard@stud.kbw.ch</a>
+                    <a href="mailto:matteo.bosshard@lernende.bbw.ch">matteo.bosshard@lernende.bbw.ch</a>
+                </div>
+
             </main>
 
             <Footer/>
