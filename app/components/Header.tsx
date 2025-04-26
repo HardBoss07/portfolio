ab062338c907e223f0afb19cc5a4e4650eedabcc
HardBoss07 <matteo@bosshard.net>
2025-04-26 22:08:32 +0200

    changed header to dynamically check the site is loaded from github pages or not and include prefix in href

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,10 +1,13 @@
 export default function Header() {
+    const isGitHubPages = typeof window !== "undefined" && window.location.hostname.includes("github.io");
+    const prefix = isGitHubPages ? "/portfolio" : "";
+
     return (
         <header>
             <div>
-                <a href="/portfolio/">Home</a>
-                <a href="/projects">Projects</a>
-                <a href="/contact">Contact</a>
+                <a href={`${prefix}/`}>Home</a>
+                <a href={`${prefix}/projects`}>Projects</a>
+                <a href={`${prefix}/contact`}>Contact</a>
                 <a href="https://github.com/HardBoss07" target="_blank">GitHub</a>
             </div>
         </header>
