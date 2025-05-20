62a3cdab5d9e340b647c49c29012900c4634cd4e
HardBoss07 <matteo@bosshard.net>
2025-05-20 14:10:09 +0200

    removed latex, added arduino since i barely have any experience with latex

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -9,7 +9,7 @@ export default function Contact() {
             <main>
                 <h1>Contact me</h1>
                 <p>
-                    If you're interested, shoot me a message with your inquiry on one of the chosen methods below. I will try to respond as quickly as possible, my timezone is GMT+1.
+                    If you're interested, shoot me a message with your inquiry on one of the chosen methods below. I will try to respond as quickly as possible.
                 </p>
                 <h2>Social Media</h2>
                 <div className="contact">
