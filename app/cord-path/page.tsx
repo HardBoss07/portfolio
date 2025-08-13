d1810eb83c59bb331da67e7b31caabfb93377d62
HardBoss07 <matteo@bosshard.net>
2025-08-13 22:15:29 +0200

    created introduction paragraph in cord-path/page.tsx

--- app/cord-path/page.tsx
+++ app/cord-path/page.tsx
@@ -9,9 +9,15 @@ export default function cordPath() {
 
             <main>
                 <div className="result-header">
-                    <h1>cord-path</h1>
+                    <h1>Cord Path</h1>
                     <RepositoryButton link="cord-path"/>
                 </div>
+                <p>
+                    Cord Path is a CLI tool that finds the optimal travel path through a set of 2D points. Cord Path
+                    uses NVIDIA CUDA to accelerate the calculations required for path finding. When given a list of
+                    points and an optional starting position, the tool returns an optimized order for the points with
+                    the shortest total distance.
+                </p>
             </main>
 
             <Footer/>
